63c7ca7ffe711eea9204570b6d93004ac7175a38
christopherdonner <christopher.donner@gmail.com>
2018-12-02 20:55:36 -0500

    2nd commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var currentCharacter
+var currentEnemy
+var hitPoints
+var enemyHitpoints
+
+
+
+$(document).ready(function() 
+{
+    $(".character").on("click", function()
+    {
+        console.log(`characterBox click ${this}`)
+    })
+}
