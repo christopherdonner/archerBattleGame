646cdf63578dde72b0e933272af8bf470cd0581b
Christopher Donner <christopher.donner@gmail.com>
2018-12-04 10:28:39 -0500

    4-12-2018-A

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,27 +16,29 @@ var character1 = {
 
 var character2 = {
     id: 1,
-    name: "Freddy",
-    hp: 120,
+    name: "Daphne",
+    hp: 100,
     currentCharacter: false,
     isCurrentEnemy: false,
 }
 
 var character3 = {
     id: 1,
-    name: "Freddy",
-    hp: 120,
+    name: "Shaggy",
+    hp: 150,
     currentCharacter: false,
     isCurrentEnemy: false,
 }
 
 var character4 = {
     id: 1,
-    name: "Freddy",
-    hp: 120,
+    name: "Velma",
+    hp: 80,
     currentCharacter: false,
     isCurrentEnemy: false,
 }
+
+var characterList=[character1, character2, character3, character4]
 
 function moveTiles() 
     {
@@ -46,26 +48,36 @@ function moveTiles()
 
 $(document).ready(function() 
 {
+
+$("#character1Name").append(character1.name)
+$("#character1HP").append(`HP: ${character1.hp}`)
+
     $(".character").on("click", function()
     {
         console.log(`test ${this.id}`)
         console.log(currentCharacter)
+
         if (currentCharacter===0)
         {
             currentCharacter=this.id;
-                if(currentCharacter===1){currentCharacter=character1;}
+            console.log(currentCharacter)
+                if(currentCharacter===1)
+                    {
+                        currentCharacter=character1;
+                        $("#character1").clone().appendTo(".current_character")}
                 else if (currentCharacter===2){currentCharacter=character2;}
                 else if (currentCharacter===3){currentCharacter=character3;}
                 else {currentCharacter=character4;}
             
             console.log(currentCharacter)
             console.log(this)
+            
             $(".character").empty();
-                for(i=1;i<5;i++)
+                for(i=1;i<characterList.length;i++)
                     {
-                        if (id!=currentCharacter)
+                        if (currentCharacter!==characterList[i])
                             {
-                                //character
+                                characterList[i]
                                 //$(`#${id}`).
                             }
                     }
