44f3bbe5fc2856607c24444f75e8975440f24bdc
Christopher Donner <christopher.donner@gmail.com>
2018-12-04 16:36:49 -0500

    characterSelection working - sortof

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var character=$(".character")
-var currentCharacter=0
+var currentCharacter=""
 var enemyCombatants=[]
 var currentEnemy
 var hitPoints
@@ -12,30 +12,35 @@ var character1 = {
     hp: 120,
     currentCharacter: false,
     currentEnemy: false,
+    isAlive: true
+
 }
 
 var character2 = {
-    id: 1,
+    id: 2,
     name: "Daphne",
     hp: 100,
     currentCharacter: false,
     isCurrentEnemy: false,
+    isAlive: true
 }
 
 var character3 = {
-    id: 1,
+    id: 3,
     name: "Shaggy",
     hp: 150,
     currentCharacter: false,
     isCurrentEnemy: false,
+    isAlive: true
 }
 
 var character4 = {
-    id: 1,
+    id: 4,
     name: "Velma",
     hp: 80,
     currentCharacter: false,
     isCurrentEnemy: false,
+    isAlive: true
 }
 
 var characterList=[character1, character2, character3, character4]
@@ -46,34 +51,79 @@ function moveTiles()
         
     }
 
+function drawCombatants()
+    {
+        for(i=0;i<enemyCombatants.length;i++)
+        {
+            
+        }
+    }
+
 $(document).ready(function() 
 {
+/*
+    for(i=0;i<characterList.length;i++)
+    {
+        console.log(characterList[i])
+        $(`#${characterList[i]}Name`).append(character1.name)
+        $(`#${characterList[i]}HP`).append(`HP: ${character1.hp}`)
+    }
+*/
 
 $("#character1Name").append(character1.name)
 $("#character1HP").append(`HP: ${character1.hp}`)
 
+$("#character2Name").append(character2.name)
+$("#character2HP").append(`HP: ${character2.hp}`)
+
+$("#character3Name").append(character3.name)
+$("#character3HP").append(`HP: ${character3.hp}`)
+
+$("#character4Name").append(character4.name)
+$("#character4HP").append(`HP: ${character4.hp}`)
+
     $(".character").on("click", function()
     {
-        console.log(`test ${this.id}`)
-        console.log(currentCharacter)
+        //console.log(`test ${this.id}`)
+        //console.log(currentCharacter)
 
-        if (currentCharacter===0)
+        if (currentCharacter==="")
         {
+            console.log(this.id)
             currentCharacter=this.id;
             console.log(currentCharacter)
-                if(currentCharacter===1)
+                if(currentCharacter==="character1")
                     {
                         currentCharacter=character1;
-                        $("#character1").clone().appendTo(".current_character")}
-                else if (currentCharacter===2){currentCharacter=character2;}
+                        enemyCombatants=["character2","character3","character4"]
+                        $("#character1").clone().appendTo(".current_character")
+                        $("#character1").empty();
+                        for(i=0;i<enemyCombatants.length;i++)
+                            {   
+                                console.log(enemyCombatants[i])
+                                $(`#${enemyCombatants[i]}`).clone().appendTo("#possibleEnemies")
+                                $(`#${enemyCombatants[i]}`).empty();
+                            }
+                    }
+                else if (currentCharacter==="character2")
+                    {
+                            currentCharacter=character2;
+                            enemyCombatants=["character1","character3","character4"]
+                            $("#character2").clone().appendTo(".current_character")
+                            $("#character1").empty();
+                            $("#character2").empty();
+                            $("#character3").empty();
+                            $("#character4").empty();
+                            
+                    }
                 else if (currentCharacter===3){currentCharacter=character3;}
                 else {currentCharacter=character4;}
             
-            console.log(currentCharacter)
-            console.log(this)
+            console.log(currentCharacter.name)
+            //console.log(this)
             
-            $(".character").empty();
-                for(i=1;i<characterList.length;i++)
+            //$(".character").empty();
+                /*for(i=1;i<characterList.length;i++)
                     {
                         if (currentCharacter!==characterList[i])
                             {
@@ -81,11 +131,12 @@ $("#character1HP").append(`HP: ${character1.hp}`)
                                 //$(`#${id}`).
                             }
                     }
+                    */
         }
     });
 });
 
     $("#attack").on("click", function()
     {
-        console.log("test")
+        console.log("attack!")
     })
